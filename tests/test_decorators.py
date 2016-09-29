03d2862814efd0f3161097609ecdbb60bbac56bc
Jan Češpivo <jan.cespivo@gmail.com>
2016-09-29 17:04:11 +0200

    proposal of new features

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -23,6 +23,39 @@ class DecoratorsTestCase(unittest.TestCase):
 
         self.assertEqual(DecoratedTests.anonymous_user.allowed_operations, {'create', 'retrieve'})
 
+    def test_same_class_decorator(self):
+
+        from rest_tests import RestTests, RestUser
+
+        class DecoratedTests(RestTests):
+            logged_user = RestUser
+
+        DecoratedTests = DecoratedTests.logged_user.can_create(DecoratedTests)
+
+        self.assertEqual(DecoratedTests.logged_user.allowed_operations, {'create'})
+
+    def test_same_class_user(self):
+
+        from rest_tests import RestTests, RestUser
+
+        class DecoratedTests(RestTests):
+            logged_user = RestUser(can_create=True)
+
+        self.assertEqual(DecoratedTests.logged_user.allowed_operations, {'create'})
+
+    def test_decorated_all_users(self):
+        from rest_tests import RestTests, RestUser
+
+        class MyTests(RestTests):
+            logged_user = RestUser
+
+        @RestTests.all_users.can_create
+        class DecoratedTests(MyTests):
+            pass
+
+        self.assertEqual(DecoratedTests.anonymous_user.allowed_operations, {'create'})
+        self.assertEqual(DecoratedTests.logged_user.allowed_operations, {'create'})
+
     def test_inheritance_decorator(self):
         from rest_tests import RestTests
 
