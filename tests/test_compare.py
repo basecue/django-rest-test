06a655c224ea33b767f780781b3ff3658e87e0be
Jan Češpivo <jan.cespivo@gmail.com>
2016-10-04 22:20:58 +0200

    tests for compare feature

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -2,7 +2,7 @@ import unittest
 from rest_tests import compare
 
 
-class BasicTestCitse(unittest.TestCase):
+class BasicTestCase(unittest.TestCase):
 
     def test_basic(self):
         data = dict(
@@ -73,7 +73,150 @@ class BasicTestCitse(unittest.TestCase):
                 c=''
             )
         )
+
+        with self.assertRaises(Exception):
+            compare(data, expected_data)
+
+    def test_basic_ellipsis(self):
+        data = dict(
+            a=1,
+            b='2'
+        )
+
+        expected_data = dict(
+            b='2',
+            a=...
+        )
+
         assert compare(data, expected_data)
 
+    def test_basic_ellipsis_false(self):
+        data = dict(
+            a=1,
+            b='2'
+        )
+        expected_data = dict(
+            b=2,
+            a=...
+        )
+
+        with self.assertRaises(Exception):
+            compare(data, expected_data)
+
+    def test_deep_ellipsis(self):
+        data = dict(
+            a=1,
+            b=dict(
+                b=dict(
+                    a='test'
+                ),
+                a=2,
+                c=''
+            )
+        )
+        expected_data = dict(
+            a=1,
+            b=dict(
+                a=2,
+                b=...,
+                c=''
+            )
+        )
+        assert compare(data, expected_data)
+
+    def test_deep_ellipsis_false(self):
+        data = dict(
+            a=1,
+            b=dict(
+                b=dict(
+                    a='test'
+                ),
+                a=2,
+                c=''
+            )
+        )
+        expected_data = dict(
+            a=1,
+            b=dict(
+                a=3,
+                b=...,
+                c=''
+            )
+        )
+
+        with self.assertRaises(Exception):
+            compare(data, expected_data)
+
+    def test_missing_basic_ellipsis(self):
+        data = dict(
+            a=1,
+            b='2'
+        )
+
+        expected_data = dict(
+            a=...
+        )
+        with self.assertRaises(Exception):
+            compare(data, expected_data)
+
+    def test_moreover_basic_ellipsis(self):
+        data = dict(
+            a=1,
+            b='2'
+        )
+        expected_data = dict(
+            b=2,
+            a=...,
+            c='test'
+        )
+
+        with self.assertRaises(Exception):
+            compare(data, expected_data)
+
+    def test_missing_deep_ellipsis(self):
+        data = dict(
+            a=1,
+            b=dict(
+                b=dict(
+                    a='test'
+                ),
+                a=2,
+                c=''
+            )
+        )
+        expected_data = dict(
+            a=1,
+            b=dict(
+                a=2,
+                b=...,
+            )
+        )
+        with self.assertRaises(Exception):
+            compare(data, expected_data)
+
+    def test_moreover_deep_ellipsis(self):
+        data = dict(
+            a=1,
+            b=dict(
+                b=dict(
+                    a='test'
+                ),
+                a=2,
+                c=''
+            )
+        )
+        expected_data = dict(
+            a=1,
+            b=dict(
+                a=3,
+                b=...,
+                c='',
+                d='test'
+            )
+        )
+
+        with self.assertRaises(Exception):
+            compare(data, expected_data)
+            
 if __name__ == '__main__':
     unittest.main()
