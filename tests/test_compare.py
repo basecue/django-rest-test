712fbecce4b30e9fce72c40b96e516d00ced14f7
Jan Češpivo <jan.cespivo@gmail.com>
2016-10-06 14:14:19 +0200

    tests for compare

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -2,7 +2,7 @@ import unittest
 from rest_tests import compare
 
 
-class BasicTestCase(unittest.TestCase):
+class DictTestCase(unittest.TestCase):
 
     def test_basic(self):
         data = dict(
@@ -213,7 +213,7 @@ class ItemEllipsisTestCase(unittest.TestCase):
         self.assertFalse(compare(data, expected_data))
 
 
-class CompleteEllipsisTestCase(unittest.TestCase):
+class DictEllipsisTestCase(unittest.TestCase):
     def test_basic(self):
         data = dict(
             a=1,
@@ -336,6 +336,245 @@ class CompleteEllipsisTestCase(unittest.TestCase):
         )
 
         self.assertFalse(compare(data, expected_data))
+
+    def test_bad_usage(self):
+        data = dict(
+            a=1,
+            b=dict(
+                b=dict(
+                    a='test'
+                ),
+                a=2,
+                c=''
+            )
+        )
+        expected_data = {
+            'a': 1,
+            ...: dict(
+                b=dict(
+                    a='test'
+                ),
+                a=2,
+                c=''
+            )
+        }
+
+        with self.assertRaises(TypeError):
+            compare(data, expected_data)
+
+
+class ListTestCase(unittest.TestCase):
+
+    def test_basic(self):
+        data = [
+            1,
+            '2'
+        ]
+        expected_data = [
+            1,
+            '2'
+        ]
+
+        assert compare(data, expected_data)
+
+    def test_basic_false(self):
+        data = [
+            1,
+            2
+        ]
+        expected_data = [
+            2,
+            1
+        ]
+
+        self.assertFalse(compare(data, expected_data))
+
+    def test_combination(self):
+        data = [
+            dict(
+                a=1,
+                b=dict(
+                    b=dict(
+                        a='test'
+                    ),
+                    a=2,
+                    c=''
+                )
+            ),
+            dict(
+                a=2,
+                b=dict(
+                    b=dict(
+                        a='test'
+                    ),
+                    a=2,
+                    c=''
+                )
+            )
+        ]
+        expected_data = [
+            dict(
+                a=1,
+                b=dict(
+                    b=dict(
+                        a='test'
+                    ),
+                    a=2,
+                    c=''
+                )
+            ),
+            dict(
+                a=2,
+                b=dict(
+                    b=dict(
+                        a='test'
+                    ),
+                    a=2,
+                    c=''
+                )
+            )
+        ]
+        assert compare(data, expected_data)
+
+
+class ListEllipsisTestCase(unittest.TestCase):
+    def test_start(self):
+        data = [
+            1,
+            2,
+            3
+        ]
+        expected_data = [
+            ...,
+            3
+        ]
+        assert compare(data, expected_data)
+
+    def test_multiple(self):
+        data = [
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+        expected_data = [
+            ...,
+            2,
+            ...
+        ]
+        assert compare(data, expected_data)
+
+    def test_end(self):
+        data = [
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+        expected_data = [
+            1,
+            ...
+        ]
+        assert compare(data, expected_data)
+
+    def test_multiple_in(self):
+        data = [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7
+        ]
+        expected_data = [
+            ...,
+            2,
+            ...,
+            5,
+            ...
+        ]
+        assert compare(data, expected_data)
+
+    def test_start_false(self):
+        data = [
+            1,
+            2,
+            3
+        ]
+        expected_data = [
+            ...,
+            4
+        ]
+        self.assertFalse(compare(data, expected_data))
+
+    def test_multiple_false(self):
+        data = [
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+        expected_data = [
+            ...,
+            6,
+            ...
+        ]
+        self.assertFalse(compare(data, expected_data))
+
+    def test_end_false(self):
+        data = [
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+        expected_data = [
+            2,
+            ...
+        ]
+        self.assertFalse(compare(data, expected_data))
+
+    def test_multiple_in_false(self):
+        data = [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7
+        ]
+        expected_data = [
+            ...,
+            2,
+            ...,
+            3,
+            ...
+        ]
+        self.assertFalse(compare(data, expected_data))
+
+    def test_bad_usage(self):
+        data = [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7
+        ]
+        expected_data = [
+            ...,
+            ...,
+            7
+        ]
+        with self.assertRaises(TypeError):
+            compare(data, expected_data)
 
 if __name__ == '__main__':
     unittest.main()
